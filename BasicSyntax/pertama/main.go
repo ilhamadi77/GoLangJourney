214d498866befb3cae19216e47eab7274a599284
ilhamadi77 <ilham.adi78@gmail.com>
2024-01-15 09:30:07 +0700

    Konsep package function

---
+++ BasicSyntax/pertama/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello I am Ready to GO developer")
+}
+
+/**
+syntax dasar di golang
+- menggunakan go run <namaFile> untuk menjalankan golang
+- menggunakan go build membuat file baru yang sudah di compile menjadi satu package yang sudah di buat sebelumnya'
+- menggunakan go mod init <NamaFolder> membuat folder root yang akan digunakan sebelum menggunakan go lang
+
+*/
+
+/**
+GO memiliki folder root ketika ingin dikerjakan, GO akan menjalakan hanya satu folder main package
+GO harus membuat package terlebih dahulu , kemudian membuat import fmt untuk menampilkan pada console. dan hanya satu file main
+
+*/
